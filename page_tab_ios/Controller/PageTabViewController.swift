b36569af1ae6083c04627953e8e7ac2bfadc3a91
dheeraj <dheerajsh123456@gmail.com>
2020-06-02 18:02:21 +0530

    Tab Detail content collection view with cells added

--- page_tab_ios/Controller/PageTabViewController.swift
+++ page_tab_ios/Controller/PageTabViewController.swift
@@ -16,10 +16,28 @@ class PageTabViewController: UIViewController {
         return v
     }()
     
+    let layout:UICollectionViewFlowLayout = UICollectionViewFlowLayout.init()
+    
+    lazy var collectionView:UICollectionView = {
+        let cv = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewLayout.init())
+        cv.delegate = self
+        cv.dataSource = self
+        cv.showsHorizontalScrollIndicator = false
+        cv.register(TabDetailContentCollectionViewCell.self, forCellWithReuseIdentifier: "TabDetailContentCollectionViewCell")
+        cv.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        cv.translatesAutoresizingMaskIntoConstraints = false
+        cv.setCollectionViewLayout(layout, animated: false)
+        cv.backgroundColor = .clear
+        cv.isPagingEnabled = true
+        return cv
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        layout.scrollDirection = .horizontal
         view.backgroundColor = .white
         view.addSubview(selectionView)
+        view.addSubview(collectionView)
         setUpConstraints()
     }
     
@@ -28,8 +46,39 @@ class PageTabViewController: UIViewController {
             selectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
             selectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             selectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            selectionView.heightAnchor.constraint(equalToConstant: 50)
+            selectionView.heightAnchor.constraint(equalToConstant: 50),
+            
+            collectionView.topAnchor.constraint(equalTo: selectionView.bottomAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
         ])
     }
 
+}
+
+extension PageTabViewController:UICollectionViewDelegate, UICollectionViewDelegateFlowLayout, UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "TabDetailContentCollectionViewCell", for: indexPath) as! TabDetailContentCollectionViewCell
+        cell.backgroundColor = indexPath.row % 2 == 0 ? .red : .blue
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width:collectionView.frame.width, height: collectionView.frame.height)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+    
 }
---
+++ page_tab_ios/View/TabDetailContentCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  TabDetailContentCollectionViewCell.swift
+//  page_tab_ios
+//
+//  Created by Dheeraj Kumar Sharma on 02/06/20.
+//  Copyright © 2020 Dheeraj Kumar Sharma. All rights reserved.
+//
+
+import UIKit
+
+class TabDetailContentCollectionViewCell: UICollectionViewCell {
+    
+}
